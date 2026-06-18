0401253f03cd2ebac496c9edf9a89a2cb7f1d9f5
Roy Crivolotti <roycrivolotti@gmail.com>
2026-06-19 00:44:45 +0200

    Remove unused HttpError import in dbWrite tests.

--- functions/_shared/dbWrite.test.ts
+++ functions/_shared/dbWrite.test.ts
@@ -1,6 +1,5 @@
 import { describe, expect, it, vi } from 'vitest'
 import { insertTransaction, updateTransaction } from './dbWrite'
-import { HttpError } from './http'
 import type { Env } from './env'
 
 function envWith(first: unknown): Env {
